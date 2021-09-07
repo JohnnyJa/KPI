e92a06a66d4dd0b9e1a25b7c66f981487bdc0903
JohnnyJa <iborovkov465@gmail.com>
2021-09-07 20:31:16 +0300

    Initial commit.

---
+++ OP/Laboratorna1/Lab1_cpp/Lab1_cpp/Lab1_cpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	float SquareBig, SquareSmall;
+	cin >> SquareBig;
+
+	SquareSmall = SquareBig / 2;
+
+	cout << SquareSmall << " 2";
+}
