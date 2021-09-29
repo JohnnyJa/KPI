c797afc6d5209d63064f66be5e0dfde2a77137df
JohnnyJa <iborovkov465@gmail.com>
2021-09-29 22:13:00 +0300

    2 лабораторна з АСД

--- OP/Laboratorna2/Lab2_cpp/Lab2_cpp/Lab2_cpp.cpp
+++ OP/Laboratorna2/Lab2_cpp/Lab2_cpp/Lab2_cpp.cpp
@@ -6,21 +6,50 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "");
+
 	float x, y, a, b, c;
+	bool res = false;
+
 	cout << "¬ведiть розмiри сторiн цеглини a,b,c: ";
 	cin >> a >> b >> c;
 
 	cout << "¬ведiть розмiри отвору: x,y: ";
 	cin >> x >> y;
 
-	if ((a <= x && b <= y) || (b <= x && a <= y) || (b <= x && c <= y) || (c <= x && b <= y) || (a <= x && c <= y) || (c <= x) && (a <= y))
+
+
+	if (a <= x && b <= y)
 	{
-		cout << "÷еглина з заданими розмiрами пройде в отвiр";
+		res = true;
 	}
+	 if (b <= x && a <= y)
+	{
+		 res = true;
+	}
+	 if (b <= x && c <= y)
+	 {
+		 res = true;
+	 }
+	 if (c <= x && b <= y)
+	 {
+		 res = true;
+	 }
+	 if (a <= x && c <= y)
+	 {
+		 res = true;
+	 }
+	 if (c <= x && a <= y)
+	 {
+		 res = true;
+	 }
+
+
+	 if (res) {
+		 cout << "÷еглина з заданими розмiрами пройде в отвiр";
+	 }
 	else
 	{
 		cout << "÷еглина з заданими розмiрами не пройде в отвiр";
 	}
-
 	return 0;
 }
--- OP/Laboratorna2/Lab2_py/Lab2_py/Lab2_py.py
+++ OP/Laboratorna2/Lab2_py/Lab2_py/Lab2_py.py
@@ -1,11 +1,25 @@
-a=float(input("Введiть розмiри сторiн цеглини: а = "))
-b=float(input("b = "))
-c=float(input("c = "))
-x=float(input("Введiть розмiри отвору: x = "))
-y=float(input("y = "))
+a = float(input("Введiть розмiри сторiн цеглини: а = "))
+b = float(input("b = "))
+c = float(input("c = "))
+x = float(input("Введiть розмiри отвору: x = "))
+y = float(input("y = "))
 
+res = False
 
-if (a <= x and b <= y) or (b <= x and a <= y) or (b <= x and c <= y) or (c <= x and b <= y) or (a <= x and c <= y) or (c <= x) and (a <= y):
+if (a <= x and b <= y): 
+    res = True
+if (b <= x and a <= y): 
+    res = True
+if (b <= x and c <= y): 
+    res = True
+if (c <= x and b <= y): 
+    res = True
+if (a <= x and c <= y): 
+    res = True
+if (c <= x and a <= y):
+    res = True
+    
+if res:
     print("Цеглина з заданими розмiрами пройде в отвiр")
 else:
-    print("Цеглина з заданими розмiрами не пройде в отвiр")
+     print("Цеглина з заданими розмiрами не пройде в отвiр")
